3187873bdf934ff9391d1a867b57970d5cf28264
v <2583695112@qq.com>
2022-08-05 22:38:48 +0800

    feat: upload image

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  ParseFilePipeBuilder,
+  HttpStatus,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import path = require('path');
 import { AppService } from './app.service';
+
+export const storage = diskStorage({
+  destination: './uploads',
+  filename: (_req, file, cb) => {
+    const filename =
+      path.parse(file.originalname.toLowerCase()).name.replace(/\s/g, '') +
+      '-' +
+      new Date().getTime();
+    const extension = path.parse(file.originalname).ext;
+    cb(null, `${filename}${extension}`);
+  },
+});
 
 @Controller()
 export class AppController {
@@ -8,5 +31,29 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  // "filedname" 对应的是 POST 提交的参数名
+  @UseInterceptors(FileInterceptor('filedname', { storage }))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'image/*',
+        })
+        .addMaxSizeValidator({
+          maxSize: 10 * 1024 * 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return {
+      statusCode: 200,
+      path: file.path,
+    };
   }
 }
